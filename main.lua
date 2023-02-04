15e6a7a59c480f6923591b3f6d070ac1c61d460e
Gustavo <gustavo.arcary@gmail.com>
2023-02-03 23:58:14 -0300

    test

--- main.lua
+++ main.lua
@@ -1,32 +1,36 @@
 Camera = require('libs.camera')
 local W, H = love.graphics.getDimensions()
+local player = require "player"
+
 
 Player = {
     x = W/2,
     y = H/2,
-    speed = 300,
+    speed = 150,
 }
 
 function love.load()
     cam = Camera()
 	cam:setFollowStyle('LOCKON')
-
+    MapW = 2000
+    MapH = 1000
+    world = love.physics.newWorld(0,0,false)
     Player.w = 10
 end
 
 function Player:move(dt)
-    if love.keyboard.isDown("w") then
-        self.y = self.y - self.speed*dt 
-    elseif love.keyboard.isDown("s") then
-        self.y = self.y + self.speed*dt 
-    elseif love.keyboard.isDown("d") then
-        self.x = self.x + self.speed*dt 
-    elseif love.keyboard.isDown("a") then
-        self.x = self.x - self.speed*dt
-    end 
+    -- if love.keyboard.isDown("w") then
+    --     self.y = self.y - self.speed*dt 
+    -- elseif love.keyboard.isDown("s") then
+    --     self.y = self.y + self.speed*dt 
+    -- elseif love.keyboard.isDown("d") then
+    --     self.x = self.x + self.speed*dt 
+    -- elseif love.keyboard.isDown("a") then
+    --     self.x = self.x - self.speed*dt
+    -- end 
 
     x, y = love.mouse.getPosition()
-    print(x,y)
+    -- print(x,y)
 
 end
 
@@ -42,13 +46,22 @@ function love.update(dt)
 	if cam.y < H/2 then
 		cam.y = H/2
 	end
+
+    if cam.x > MapW/2 then
+		cam.x = MapW/2
+	end
+
+	if cam.y > MapH/2 then
+		cam.y = MapH/2
+	end
 end
 
 function love.draw()
     cam:attach()
     love.graphics.setColor(0.5,0.5,1)
-    love.graphics.rectangle("fill",0,0,2000,1000)
+    love.graphics.rectangle("fill",0,0,MapW,MapH)
     love.graphics.setColor(1,0.5,0)
     love.graphics.circle('fill',Player.x,Player.y,Player.w)
+    love.graphics.rectangle('fill',20,20,20,500)
     cam:detach()
 end
---
+++ player.lua
@@ -0,0 +1,43 @@
+local player = {} 
+
+function player.load()
+    player.body = love.
+
+
+
+
+
+end
+
+
+function player.update(dt)
+
+
+
+
+
+end
+
+function player.draw()
+
+
+
+
+end
+
+function player.keypressed(key)
+
+
+
+
+end
+
+function player.keyreleased(key)
+    
+end
+
+function player.mousepressed(x,y,button)
+
+end
+
+return player
