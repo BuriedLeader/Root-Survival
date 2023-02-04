e07468c0edb758909bbdd19fbac8793fa3bc0f43
Gustavo <gustavo.arcary@gmail.com>
2023-02-04 02:07:34 -0300

    jogador_mundo

--- main.lua
+++ main.lua
@@ -2,23 +2,40 @@ Camera = require('libs.camera')
 local W, H = love.graphics.getDimensions()
 local player = require "player"
 
+local parede = {}
 
-Player = {
-    x = W/2,
-    y = H/2,
-    speed = 150,
-}
+azul_mar = {89/255,215/255,1}
+-- Player = {
+--     x = W/2,
+--     y = H/2,
+--     speed = 150,
+-- }
+function objetos_staticos (x,y,w,h,mundo)
+    local objeto = {
+      x = x,
+      y = y,
+      w = w,
+      h = h
+    }   
+    objeto.body = love.physics.newBody(mundo,x,y,"static")
+    objeto.shape = love.physics.newPolygonShape(0,0,0,h,w,0,w,h)
+    objeto.fixture = love.physics.newFixture(objeto.body,objeto.shape,1)
+    return objeto
+end
 
 function love.load()
     cam = Camera()
 	cam:setFollowStyle('LOCKON')
     MapW = 2000
     MapH = 1000
-    world = love.physics.newWorld(0,0,false)
-    Player.w = 10
+    world = love.physics.newWorld(0,0,true)
+
+    parede[1] = objetos_staticos(200,350,200,75,world)
+    player.load()
+    -- Player.w = 10
 end
 
-function Player:move(dt)
+--function Player:move(dt)
     -- if love.keyboard.isDown("w") then
     --     self.y = self.y - self.speed*dt 
     -- elseif love.keyboard.isDown("s") then
@@ -29,15 +46,18 @@ function Player:move(dt)
     --     self.x = self.x - self.speed*dt
     -- end 
 
-    x, y = love.mouse.getPosition()
+    --x, y = love.mouse.getPosition()
     -- print(x,y)
 
-end
+--end
 
 function love.update(dt)
-    Player:move(dt)
+    world:update(dt)
+    if dt > 0.040 then return end
+    -- Player:move(dt)
+    px ,py = player.body:getPosition()  
     cam:update(dt)
-	cam:follow(Player.x,Player.y)
+	cam:follow(px,py)
 
     if cam.x < W/2 then
 		cam.x = W/2
@@ -54,6 +74,17 @@ function love.update(dt)
 	if cam.y > MapH/2 then
 		cam.y = MapH/2
 	end
+
+    player.update(dt)
+end
+
+function desenha_cenario(cenario,cor,translucidez)
+    love.graphics.setColor(cor,translucidez or 1)
+    for i = 1, #cenario do
+      if cenario[i].body:isDestroyed() == false then
+        love.graphics.polygon("fill",cenario[i].body:getWorldPoints(cenario[i].shape:getPoints()))
+      end
+    end
 end
 
 function love.draw()
@@ -61,7 +92,9 @@ function love.draw()
     love.graphics.setColor(0.5,0.5,1)
     love.graphics.rectangle("fill",0,0,MapW,MapH)
     love.graphics.setColor(1,0.5,0)
-    love.graphics.circle('fill',Player.x,Player.y,Player.w)
-    love.graphics.rectangle('fill',20,20,20,500)
+    --love.graphics.circle('fill',Player.x,Player.y,Player.w)
+    -- love.graphics.rectangle('fill',20,20,20,500)
+    desenha_cenario(parede,azul_mar)
+    player.draw()
     cam:detach()
 end
--- player.lua
+++ player.lua
@@ -1,43 +1,61 @@
 local player = {} 
+local W, H = love.graphics.getDimensions()
 
 function player.load()
-    player.body = love.
-
-
-
-
-
+    radius = 10
+    player.body = love.physics.newBody(world,W/2,H/2,'dynamic')
+    player.shape = love.physics.newCircleShape(radius)
+    player.fixture = love.physics.newFixture(player.body,player.shape,1)
+    player.fixture:setFriction(0)
+    player.body:setFixedRotation(true)
 end
 
+function move (letra)
+    if love.keyboard.isDown(letra) then
+        return true
+    end
+    return false
+end
+
+function player_move(dt)
+    local velocidade = 150
+    move_x = 0
+    move_y = 0
+
+    if move('a') then
+        move_x = move_x - 1
+    end
+
+    if move('d') then
+        move_x = move_x + 1
+    end
+
+    if move('w') then
+        move_y = move_y - 1
+    end
+
+    if move('s') then
+        move_y = move_y + 1
+    end
+
+    if move_x ~= 0 and move_y ~= 0 then
+        local normalize = math.sqrt(2)
+        move_x = move_x / normalize
+        move_y = move_y / normalize
+    end
+
+    player.body:setLinearVelocity(move_x*velocidade,move_y*velocidade)
+end
 
 function player.update(dt)
-
-
-
-
-
+    player.body:setLinearVelocity(150,200)
+    player_move()
 end
 
 function player.draw()
-
-
-
-
-end
-
-function player.keypressed(key)
-
-
-
-
-end
-
-function player.keyreleased(key)
-    
-end
-
-function player.mousepressed(x,y,button)
-
+    x,y = player.body:getPosition()
+    love.graphics.setColor(0,1,0)
+    love.graphics.circle("fill",x,y,radius)
 end
 
 return player
