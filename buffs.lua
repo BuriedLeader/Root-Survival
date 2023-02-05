e1db909a5f14473ba6c0251a42c2ece2efa8adf6
ViniMRV <buriedleader@gmail.com>
2023-02-05 14:50:00 -0300

    corrigi os bugs

--- buffs.lua
+++ buffs.lua
@@ -7,34 +7,40 @@ buffs = {
     HealthIncrease = {
         type = "buff",
         activated = false,
-        effect = function (player) player.actual_HP = player.actual_HP + player.actual_HP/10 end,
+        effect = function (player) 
+            player.base_HP = player.base_HP*1.1
+            player.life = player.life + player.base_HP*0.1
+        end
     },
     SpeedIncrease = {
         type = "buff",
         activated = false,
-        effect = function (player)  player.actual_speed = player.actual_speed + player.actual_speed/10 end
+        effect = function (player)  player.actual_speed = player.actual_speed*1.1 end
     },
     DamageIncrease = {
        type = "buff",
        activated = false,
-       effect = function (player)  player.actual_damage = player.actual_damage + player.actual_damage/10 end
+       effect = function (player)  player.actual_damage = player.actual_damage*1.1 end
     },
     SuperDamageIncrease = {
        type = "buff",
        activated = false,
-       effect = function (player)  player.actual_damage = player.actual_damage + player.actual_damage*0.33 end
+       effect = function (player)  player.actual_damage = player.actual_damage*1.3 end
     },
 
     SuperHealthIncrease = {
        type = "buff",
        activated = false,
-       effect = function (player)   player.actual_HP = player.actual_speed + player.actual_speed*0.33 end
+       effect = function (player)  
+            player.base_HP = player.base_HP*1.3
+            player.life = player.life + player.base_HP*0.3
+        end
     },
 
     SuperSpeedIncrease = {
        type = "buff",
        activated = false,
-       effect = function (player)  player.actual_speed = player.actual_speed + player.actual_speed*0.33 end
+       effect = function (player)  player.actual_speed = player.actual_speed*1.3 end
     }
 
 
